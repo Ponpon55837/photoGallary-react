c0dd0ed8187459b7bd5a5ca808b29de0ac215d48
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-07-31 08:43:43 +0800

    add bootstrap to use

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,29 +1,31 @@
 import React from 'react'
 import useFirestore from '../hooks/useFireStore.js'
 import { motion } from 'framer-motion'
+import { Row, Col } from 'react-bootstrap'
 
 const ImageGrid = ({ setSelectedImg }) => {
   const { docs } = useFirestore('images')
   console.log(docs)
 
   return (
-    <div className='img-grid'>
+    <Row className='img-grid'>
       { docs && docs.map(doc => (
-        <motion.div
-          className='img-wrap'
-          key={doc.id}
-          layout
-          whileHover={{ opacity: 1 }}
-          onClick={() => setSelectedImg(doc.url)}>
-          <motion.img
-            src={doc.url}
-            alt='upload pic'
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            tramsition={{ delay: 1 }} />
-        </motion.div>
+        <Col xs={12} sm={12} md={6} lg={4} xl={3} key={doc.id}>
+          <motion.div
+            className='img-wrap'
+            layout
+            whileHover={{ opacity: 1 }}
+            onClick={() => setSelectedImg(doc.url)}>
+            <motion.img
+              src={doc.url}
+              alt='upload pic'
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              tramsition={{ delay: 1 }} />
+          </motion.div>
+        </Col>
       ))}
-    </div>
+    </Row>
   )
 }
 
